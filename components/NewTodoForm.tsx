82fcb0921cc1a832227407927311b875521f9adc
mcnoble1 <idowufestustemiloluwa@gmail.com>
2024-05-07 10:33:58 +0100

    feat: created todo components :octocat:

---
+++ components/NewTodoForm.tsx
@@ -0,0 +1,10 @@
+const NewTodoForm = () => {
+    return (
+        <form>
+            <input type="text" className="border border-black/25"/>
+            <button type="submit">Add</button>
+        </form>
+    );
+}
+
+export default NewTodoForm;
